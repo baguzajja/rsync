648eaa4c8a90a9fc098d80cc857633b227655b8c
Baguz A Wibowo <baguz@malangtech.com>
2014-12-08 22:18:10 +0700

    Script for cloning website file
    Script for cloning website file

---
+++ rsync.sh
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1343\cocoasubrtf160
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+
+\f0\fs24 \cf0 #!/bin/sh\
+rsync -avz -u --progress -e "ssh -p 22\'94  user@iptremote:/home/user/public_html /home/user/\
+chown -R www-data:www-data /home/user/public_html\
+chmod 755 /home/user/public_html\
+}
